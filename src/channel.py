30b851b7a211efcfd5595d3099fe17e70771da0c
Maxim Madunts <max.madunts@gmail.com>
2024-01-27 12:06:12 +0300

    working on code

--- src/channel.py
+++ src/channel.py
@@ -4,7 +4,7 @@ from googleapiclient.discovery import build
 os.environ["API_KEY"] = "AIzaSyBwgpmMH0dA4JQSAvcf0Li8pxKvlM4oA5g"
 class Channel:
     def __init__(self, channel_id):
-        self.api_key = os.environ.get("YOUTUBE_API_KEY")
+        self.api_key = os.environ.get("API_KEY")
         self.channel_id = channel_id
         self.youtube = build("youtube", "v3", developerKey=self.api_key)
 
@@ -25,6 +25,6 @@ class Channel:
         print(f"Videos: {videos}")
 
 if __name__ == "__main__":
-    channel_id = "здесь_ваш_ID_канала"
+    channel_id = "AIzaSyBwgpmMH0dA4JQSAvcf0Li8pxKvlM4oA5g"
     channel = Channel(channel_id)
     channel.print_info()
